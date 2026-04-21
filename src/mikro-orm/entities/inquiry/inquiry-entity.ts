8f0a6b75fd149ab4496e7022cd8f29f977065470
Hijiji <wlals1169@naver.com>
2026-04-21 15:43:07 +0900

    inquiryEntity 수정

--- src/mikro-orm/entities/inquiry/inquiry-entity.ts
+++ src/mikro-orm/entities/inquiry/inquiry-entity.ts
@@ -1,15 +1,36 @@
 import { Entity, Enum, PrimaryKey } from '@mikro-orm/core';
 import { IndustryType } from 'src/enums/industry-type.enum';
+import { Column, CreateDateColumn } from 'typeorm';
 
 @Entity({ tableName: 'inquiry' })
 export class InquiryEntity {
   @PrimaryKey({ name: 'id' })
   id?: number;
 
-  @Enum({ items: () => IndustryType, comment: '업종' })
+  @Column({
+    type: 'varchar',
+    length: 30, // Enum 값 중 가장 긴 것보다 넉넉하게 설정
+    comment: '업종',
+    nullable: false,
+  })
   industry: IndustryType;
 
-  /**
-   * TODO : 구현 필요
-   */
+  @Column({
+    name: 'phoneNumber',
+    length: 20,
+    comment: '전화번호',
+    nullable: false,
+  })
+  phoneNumber: string;
+
+  @Column({
+    name: 'businessNumber',
+    length: 20,
+    comment: '사업자번호',
+    nullable: true,
+  })
+  businessNumber?: string;
+
+  @CreateDateColumn({ name: 'createdAt', comment: '생성일', nullable: false })
+  createdAt: Date;
 }
