0dab6835f66036348a3b6a2a54eb777c71e1e353
Hijiji <wlals1169@naver.com>
2026-04-22 02:47:37 +0900

    StoreModule추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,14 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
-import { SqliteDriver } from '@mikro-orm/sqlite';
-import { Module } from '@nestjs/common';
+import { MikroORM, SqliteDriver } from '@mikro-orm/sqlite';
+import { Module, OnModuleInit } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { InternalApiModule } from './api/internal/internal-api.module';
 import { PublicApiModule } from './api/public/public-api.module';
 import { DB_NAME } from './mikro-orm/const';
 import { InquiryEntity } from './mikro-orm/entities/inquiry/inquiry-entity';
 import { BatchModule } from './batch/batch.module';
+import { StoreEntity } from './mikro-orm/entities/store/store-entity';
+import { StoreModule } from './api/store/store.module';
 
 @Module({
   imports: [
@@ -18,11 +20,21 @@ import { BatchModule } from './batch/batch.module';
       dbName: DB_NAME,
       driver: SqliteDriver,
       allowGlobalContext: true,
-      entities: [InquiryEntity],
+      entities: [InquiryEntity, StoreEntity],
     }),
     InternalApiModule,
     PublicApiModule,
     BatchModule,
+    StoreModule,
   ],
 })
-export class AppModule {}
+export class AppModule implements OnModuleInit {
+  constructor(private readonly orm: MikroORM) {}
+
+  async onModuleInit() {
+    // 서버가 뜰 때 DB 스키마를 자동으로 업데이트/생성합니다.
+    const generator = this.orm.getSchemaGenerator();
+    await generator.updateSchema();
+    console.log('✅ SQLite Database Schema Sync Completed!');
+  }
+}
