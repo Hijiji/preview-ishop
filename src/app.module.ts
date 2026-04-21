b300b468db16ea1ee03cc8417e467c517d59eeed
Hijiji <wlals1169@naver.com>
2026-04-21 15:15:22 +0900

    env 파일 및 설정 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { SqliteDriver } from '@mikro-orm/sqlite';
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { InternalApiModule } from './api/internal/internal-api.module';
 import { PublicApiModule } from './api/public/public-api.module';
 import { DB_NAME } from './mikro-orm/const';
@@ -9,6 +10,10 @@ import { BatchModule } from './batch/batch.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
     MikroOrmModule.forRoot({
       dbName: DB_NAME,
       driver: SqliteDriver,
