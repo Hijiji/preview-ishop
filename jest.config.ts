c3ea0e1b17d7da1da9f392c2ff95a9eb90e1e0e9
Hijiji <wlals1169@naver.com>
2026-04-21 16:22:11 +0900

    jest install

---
+++ jest.config.ts
@@ -0,0 +1,16 @@
+const config = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  moduleFileExtensions: ['js', 'json', 'ts'],
+  rootDir: '.',
+  testRegex: '.*\\.spec\\.ts$',
+  transform: {
+    '^.+\\.(t|j)s$': ['ts-jest', { tsconfig: 'tsconfig.json' }],
+  },
+  moduleNameMapper: {
+    '^src/(.*)$': '<rootDir>/src/$1',
+  },
+  collectCoverage: false,
+};
+
+export default config;
